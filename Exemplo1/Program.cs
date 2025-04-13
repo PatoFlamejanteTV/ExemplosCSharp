2abc113c6c94e7db3b637a28d5d80c9c87e99a40
UltimateQuack <patoflamejantetv@gmail.com>
2025-04-13 16:05:11 -0300

    Adição: Novo exemplo (Exemplo2)

--- Exemplo1/Program.cs
+++ Exemplo1/Program.cs
@@ -1,10 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// Pergunta o nome do usuário
-Console.Write("Insira seu nome: ");
+﻿// Pergunta o nome do usuário
+Console.Write(
+    "Insira seu nome: "
+    );
 
 // Lê o que o usuário digitar depois da pergunta
 string? name = Console.ReadLine();
 
 // Exibe "Olá! [nome]!"
-Console.WriteLine($"Olá! {name}!");
+Console.WriteLine(
+    $"Olá! {name}!"
+    );
---
+++ Exemplo2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Método principal
+Console.WriteLine("Obtendo o nome de usuário...");
+
+// Obtém o nome de usuário do ambiente
+string nomeUsuario = Environment.UserName;
+//                   Environment SIGNIFICA O COMPUTADOR
+
+// Exibe o nome de usuário
+Console.WriteLine($"Nome de usuário do computador: {nomeUsuario}");
+
+// Aguarda input do usuário antes de fechar
+Console.WriteLine("\nPressione qualquer tecla para sair...");
+Console.ReadKey(); // não fecha até apertar qualquer tecla.
