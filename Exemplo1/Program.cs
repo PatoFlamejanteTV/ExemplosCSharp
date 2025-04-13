f5c58f36c5ff79ceb5af3ecad0c26705e84a7e32
UltimateQuack <patoflamejantetv@gmail.com>
2025-04-13 15:53:02 -0300

    Adicionado uns coiso

---
+++ Exemplo1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Pergunta o nome do usuário
+Console.Write("Insira seu nome: ");
+
+// Lê o que o usuário digitar depois da pergunta
+string? name = Console.ReadLine();
+
+// Exibe "Olá! [nome]!"
+Console.WriteLine($"Olá! {name}!");
